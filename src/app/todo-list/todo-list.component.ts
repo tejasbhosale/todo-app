5750c065c5559817371b429344982d02224cf6f5
Tejas B <tejas.tanaji@gmail.com>
2018-11-10 17:39:09 -0800

    added priority stylings and logic

--- src/app/todo-list/todo-list.component.ts
+++ src/app/todo-list/todo-list.component.ts
@@ -9,14 +9,22 @@ export class TodoListComponent implements OnInit {
   val: string;
   viewValue: string;
   todoitems: any;
+  iterm: any;
   minDate = new Date(2000, 0, 1);
   maxDate = new Date(2020, 0, 1);
+  planModel: any = {
+    start_time: new Date()
+  };
+  setPriority: boolean;
+  color: string;
 
   priority: any[] = [
     { val: 'low', viewValue: 'Low' },
-    { val: 'med', viewValue: 'Medium' },
+    { val: 'medium', viewValue: 'Medium' },
     { val: 'high', viewValue: 'High' }
   ];
+  selectedValue: string = this.priority[0].val;
+  selected = 'low';
 
   constructor() { }
 
@@ -25,9 +33,14 @@ export class TodoListComponent implements OnInit {
   }
   value = '';
   onEnter(value: string) {
+
     if (value.length > 0) {
       this.value = value;
-      this.todoitems.push(value);
+      let item = {
+        "value": value,
+        "selected": this.selected
+      };
+      this.todoitems.push(item);
     }
   }
 }
